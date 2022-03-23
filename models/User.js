4fce680d911020fb364891576468f8a18e8c131d
Nicolas Del Valle <adrianchelo7@gmail.com>
2022-03-23 19:01:52 -0300

    User model + conecction to db

---
+++ models/User.js
@@ -0,0 +1,40 @@
+module.exports = (sequelize, Model, DataTypes) => {
+  class User extends Model {}
+
+  User.init(
+    {
+      id: {
+        type: DataTypes.BIGINT.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      firstname: {
+        type: DataTypes.STRING,
+      },
+      lastname: {
+        type: DataTypes.STRING,
+      },
+      email: {
+        type: DataTypes.STRING,
+      },
+      password: {
+        type: DataTypes.STRING,
+      },
+      adress: {
+        type: DataTypes.STRING,
+      },
+      phone: {
+        type: DataTypes.STRING,
+      },
+      lastname: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      sequelize,
+      modelName: "user",
+    }
+  );
+
+  return User;
+};
