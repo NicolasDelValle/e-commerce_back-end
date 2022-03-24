05ecfd8c518436e4d44eab0f18679af69dc7c518
Charlygaf <charlygaf@gmail.com>
2022-03-23 21:12:37 -0300

    Category, relaciones, sequelize export

---
+++ models/Category.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, Model, DataTypes) => {
+  class Category extends Model {}
+
+  Category.init(
+    {
+      id: {
+        type: DataTypes.BIGINT.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      sequelize,
+      modelName: "category",
+    }
+  );
+
+  return Category;
+};
--- models/index.js
+++ models/index.js
@@ -10,3 +10,15 @@ const sequelize = new Sequelize(
     logging: false, // Para que no aparezcan mensajes en consola.
   }
 );
+
+const User = require("./User")(sequelize, Model, DataTypes);
+const Product = require("./Product")(sequelize, Model, DataTypes);
+const Order = require("./Order")(sequelize, Model, DataTypes);
+const Category = require("./Category")(sequelize, Model, DataTypes);
+
+User.hasMany(Order);
+Order.belongsTo(User);
+Category.hasMany(Product);
+Order.hasMany(Product);
+
+// module.exports = {sequelize,User,Product,Category,Order,};
