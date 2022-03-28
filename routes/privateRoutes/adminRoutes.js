af2734226106ab16e82ef177901b11832777344b
Luis Zunino <zuninoluis01@gmail.com>
2022-03-28 11:13:45 -0300

    se completan rutas de admin routes

--- routes/privateRoutes/adminRoutes.js
+++ routes/privateRoutes/adminRoutes.js
@@ -2,6 +2,19 @@ const express = require("express");
 const adminRouter = express.Router();
 
 // Rutas del Públicas:
-// ...
+// Listar todos los usuarios
+adminRouter.get("/admin/users");
+//Listar un usario
+adminRouter.get("/admin/users/:id");
+// Editar un usuario
+adminRouter.put("/admin/users/:id");
+//Borrar un usuario
+adminRouter.delete("/admin/users/:id");
+// Crea un producto
+adminRouter.post("/admin/products");
+// Borra un producto
+adminRouter.delete("/admin/admin/products/:slug");
+// Edita un producto
+adminRouter.put("/admin/products/:slug");
 
 module.exports = adminRouter;
