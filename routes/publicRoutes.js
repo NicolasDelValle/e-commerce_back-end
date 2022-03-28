6071bf2c062620af4b31b565e80fa806e3f7af60
Luis Zunino <zuninoluis01@gmail.com>
2022-03-28 11:08:05 -0300

    se suben rutass pulblicas

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -2,6 +2,12 @@ const express = require("express");
 const publicRouter = express.Router();
 
 // Rutas del Públicas:
-// ...
+publicRouter.post("/users");
+
+publicRouter.post("/tokens");
+
+publicRouter.get("/products");
+
+publicRouter.get("/products/:slug");
 
 module.exports = publicRouter;
