0d653b7dbd5db6f1667e925d069832850f8c46b3
Luis Zunino <zuninoluis01@gmail.com>
2022-03-29 13:34:23 -0300

    se aniade un nuevo metodo en el modelo
    se agrega otro beforeCreate para que uando se registre me hashe

--- models/User.js
+++ models/User.js
@@ -2,65 +2,68 @@ const { DataTypes } = require("sequelize");
 const bcrypt = require("bcryptjs");
 
 module.exports = (sequelize, Model) => {
-	class User extends Model {
-		async validatePassword(password) {
-			return await bcrypt.compare(password, this.password);
-		}
-	}
+  class User extends Model {
+    async validatePassword(password) {
+      return await bcrypt.compare(password, this.password);
+    }
+  }
 
-	User.init(
-		{
-			id: {
-				type: DataTypes.BIGINT,
-				primaryKey: true,
-				autoIncrement: true,
-			},
-			firstname: {
-				type: DataTypes.STRING,
-				allowNull: false,
-			},
-			lastname: {
-				type: DataTypes.STRING,
-				allowNull: false,
-			},
-			email: {
-				type: DataTypes.STRING,
-				allowNull: false,
-				isEmail: true,
-				unique: true,
-			},
-			password: {
-				type: DataTypes.STRING,
-				allowNull: false,
-			},
-			address: {
-				type: DataTypes.STRING,
-				allowNull: false,
-			},
-			phone: {
-				type: DataTypes.BIGINT,
-				allowNull: false,
-				unique: true,
-			},
-			orderList: {
-				type: DataTypes.JSON,
-			},
-			isAdmin: {
-				type: DataTypes.BOOLEAN,
-				allowNull: false,
-				default: false,
-			},
-		},
-		{
-			sequelize,
-			modelName: "user",
-		}
-	);
+  User.init(
+    {
+      id: {
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      firstname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        isEmail: true,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      phone: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+        unique: true,
+      },
+      orderList: {
+        type: DataTypes.JSON,
+      },
+      isAdmin: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        default: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "user",
+    }
+  );
 
-	User.beforeBulkCreate(async (users, options) => {
-		for (const user of users) {
-			user.password = await bcrypt.hash(user.password, 10);
-		}
-	});
-	return User;
+  User.beforeBulkCreate(async (users, options) => {
+    for (const user of users) {
+      user.password = await bcrypt.hash(user.password, 10);
+    }
+  });
+  User.beforeCreate(async (user, options) => {
+    user.password = await bcrypt.hash(user.password, 10);
+  });
+  return User;
 };
