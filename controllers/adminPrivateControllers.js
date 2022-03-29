3f2cf22c616a402dd878b353d0a4ed0e25f1dde8
Luis Zunino <zuninoluis01@gmail.com>
2022-03-29 16:44:34 -0300

    se finaliza el patch de admin

--- controllers/adminPrivateControllers.js
+++ controllers/adminPrivateControllers.js
@@ -18,7 +18,29 @@ async function show(req, res) {
     res.status(404).json("Ocurrio un error");
   }
 }
+async function update(req, res) {
+  const { isAdmin } = req.body;
+  const { id } = req.params;
+  try {
+    const userUpdated = await User.findByPk(id);
+    console.log(userUpdated);
+    if (!userUpdated) {
+      res.status(404).json({ message: `No existe user` });
+    } else {
+      userUpdated.isAdmin = !userUpdated.isAdmin;
+      userUpdated.save();
+
+      res.status(200).json(userUpdated);
+    }
+  } catch (err) {
+    res.status(404).json("No se encuentra" + " " + err);
+  }
+}
+
+async function destroy(req, res) {}
 module.exports = {
   index,
   show,
+  update,
+  destroy,
 };
--- routes/privateRoutes/adminRoutes.js
+++ routes/privateRoutes/adminRoutes.js
@@ -1,20 +1,25 @@
 const express = require("express");
 const adminRouter = express.Router();
-const { index, show } = require("../../controllers/adminPrivateControllers");
+const {
+  index,
+  show,
+  update,
+  destroy,
+} = require("../../controllers/adminPrivateControllers");
 
 // Listar todos los usuarios
 adminRouter.get("/users", index);
 //Listar un usario
 adminRouter.get("/users/:id", show);
 // Editar un usuario
-adminRouter.put("/users/:id");
+adminRouter.patch("/users/:id", update);
 //Borrar un usuario
-adminRouter.delete("/users/:id");
+adminRouter.delete("/users/:id", destroy);
 // Crea un producto
 adminRouter.post("/products");
 // Borra un producto
 adminRouter.delete("/products/:slug");
 // Edita un producto
-adminRouter.put("/products/:slug");
+adminRouter.patch("/products/:slug");
 
 module.exports = adminRouter;
