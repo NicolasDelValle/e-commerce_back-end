f53a83ad703df08ae915b7f70f3cfcd793584b5a
Luis Zunino <zuninoluis01@gmail.com>
2022-03-29 13:34:33 -0300

    se finaliza metodo register

--- controllers/publicControllers.js
+++ controllers/publicControllers.js
@@ -13,4 +13,31 @@ async function showProduct(req, res) {
   res.json(product);
 }
 
-module.exports = { showProducts, showProduct };
+async function register(req, res) {
+  try {
+    const { firstname, lastname, email, address, number, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (user) {
+      res
+        .status(402)
+        .json({ message: `El usuario con correo ${email} ya existe` });
+    } else {
+      await User.create({
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        address: address,
+        phone: number,
+        password: password,
+        isAdmin: false,
+      });
+      const newUser = await User.findOne({ where: { email } });
+      res.json(newUser);
+    }
+  } catch (err) {
+    res.status(404).json({ message: `Algo pasó ${err}` });
+    console.log(err);
+  }
+}
+
+module.exports = { showProducts, showProduct, register };
