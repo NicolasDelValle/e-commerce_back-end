82bdc9093afb166debc4c479a2cc967fc4359705
Luis Zunino <zuninoluis01@gmail.com>
2022-03-29 15:33:46 -0300

    se continua con el CRUD de admin

--- controllers/adminPrivateControllers.js
+++ controllers/adminPrivateControllers.js
@@ -1,16 +1,23 @@
 const { User } = require("../models");
 
 async function index(req, res) {
-  const { id } = req.user;
-  const user = await User.findByPk(id);
-  if (user.isAdmin) {
-    const users = await User.findAll();
-    res.json(users);
+  const users = await User.findAll();
+  if (users) {
+    res.status(200).json(users);
   } else {
-    res.json("No es admin");
+    res.status(404).json("Ocurrio un error");
   }
 }
 
+async function show(req, res) {
+  const { id } = req.params;
+  const user = await User.findByPk(id);
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    res.status(404).json("Ocurrio un error");
+  }
+}
 module.exports = {
   index,
   show,
