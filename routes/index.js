6e3ec463f60296caba8d78efd2b452da11e0b9f7
Luis Zunino <zuninoluis01@gmail.com>
2022-03-29 22:40:03 -0300

    se coloca jwt

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const publicRoutes = require("./publicRoutes");
 const adminRoutes = require("./privateRoutes/adminRoutes");
 const userRoutes = require("./privateRoutes/userRoutes");
+const checkJwt = require("express-jwt");
 
 module.exports = (app) => {
   app.use(publicRoutes);
+  app.use(
+    checkJwt({ secret: process.env.TOKEN_SECRET, algorithms: ["HS256"] })
+  );
   app.use("/admin", adminRoutes);
   app.use(userRoutes);
 };
--- routes/privateRoutes/userRoutes.js
+++ routes/privateRoutes/userRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const userRouter = express.Router();
 
-// Rutas del Públicas:
 //Realizar un pedido
 userRouter.post("/order");
 // Muestra un pedido
