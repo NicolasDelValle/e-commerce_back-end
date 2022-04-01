593553e0893d624bec1469e9f40f3018411bc438
Luis Zunino <zuninoluis01@gmail.com>
2022-04-01 11:41:55 -0300

    se modfica mensajes de las respuestas

--- controllers/adminPrivateControllers.js
+++ controllers/adminPrivateControllers.js
@@ -5,7 +5,7 @@ async function index(req, res) {
   if (users) {
     res.status(200).json(users);
   } else {
-    res.status(404).json("Ocurrio un error");
+    res.status(404).json("No se ha encontrado la lista de usuarios indicado");
   }
 }
 
@@ -15,17 +15,16 @@ async function show(req, res) {
   if (user) {
     res.status(200).json(user);
   } else {
-    res.status(404).json("Ocurrio un error");
+    res.status(404).json("No se ha encontrado el usuario indicado");
   }
 }
 async function updateUser(req, res) {
-  const { isAdmin } = req.body;
   const { id } = req.params;
   try {
     const userUpdated = await User.findByPk(id);
 
     if (!userUpdated) {
-      res.status(404).json({ message: `No existe user` });
+      res.status(404).json({ message: `No existe usuario indicado` });
     } else {
       userUpdated.isAdmin = !userUpdated.isAdmin;
       userUpdated.save();
@@ -42,12 +41,12 @@ async function destroyUser(req, res) {
     const { id } = req.params;
     const user = await User.findByPk(id);
     if (user) {
-      const userDelete = await User.destroy({
+      await User.destroy({
         where: { id: id },
       });
-      res.status(200).json("El siguiente usuario fue eliminado" + userDelete);
+      res.status(200).json({ message: `El usuario fue eliminado` });
     } else {
-      res.status(404).json("El usuario indicado no existe");
+      res.status(404).json({ message: "El usuario indicado no existe" });
     }
   } catch (err) {
     res
@@ -61,7 +60,9 @@ async function store(req, res) {
     const newProduct = await Product.create(req.body);
     res.status(200).json(newProduct);
   } else {
-    res.status(400).json("Oucrrio un error al momento de crear el producto");
+    res
+      .status(400)
+      .json({ message: "Oucrrio un error al momento de crear el producto" });
   }
 }
 
@@ -72,7 +73,9 @@ async function destroyProduct(req, res) {
 
   if (productDelete) {
     const productDelete = await Product.destroy({ where: { slug: slug } });
-    res.status(200).json("El siguiente producto fue eliminado" + productDelete);
+    res
+      .status(200)
+      .json({ message: "El siguiente producto fue eliminado" + productDelete });
   } else {
     res.status(404).json({ message: "Producto no encontrado" });
   }
