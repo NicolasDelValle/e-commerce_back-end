d3be0146d8cbe32b56519976ca7863f9bf254184
Luis Zunino <zuninoluis01@gmail.com>
2022-04-06 14:52:08 -0300

    puse un try y catch por si ocurre un error continue

--- controllers/apiTokenController.js
+++ controllers/apiTokenController.js
@@ -2,14 +2,18 @@ const { User } = require("../models");
 const jwt = require("jsonwebtoken");
 
 async function createToken(req, res) {
-  const { password, email } = req.body;
-  const user = await User.findOne({ where: { email: email } });
+  try {
+    const { password, email } = req.body;
+    const user = await User.findOne({ where: { email: email } });
 
-  if (user && (await user.validatePassword(password))) {
-    const newToken = jwt.sign({ id: user.id }, process.env.TOKEN_SECRET);
-    res.json({ newToken, id: user.id, isAdmin: user.isAdmin });
-  } else {
-    res.status(401).json({ message: "Ese email o contraseña es incorrecto" });
+    if (user && (await user.validatePassword(password))) {
+      const newToken = jwt.sign({ id: user.id }, process.env.TOKEN_SECRET);
+      res.json({ newToken, id: user.id, isAdmin: user.isAdmin });
+    } else {
+      res.status(401).json({ message: "Ese email o contraseña es incorrecto" });
+    }
+  } catch (error) {
+    res.status(401).json({ message: "Ocurrio un error" });
   }
 }
 
