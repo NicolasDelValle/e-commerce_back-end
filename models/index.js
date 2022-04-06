33236e23fd4fd9fcc94fabaf3faa803790381d23
mauroraviolo23 <mauro.raviolo@gmail.com>
2022-04-06 00:24:51 -0300

    add admin controller for orders

--- models/index.js
+++ models/index.js
@@ -1,15 +1,15 @@
 const { Sequelize, Model } = require("sequelize");
 
 const sequelize = new Sequelize(
-  process.env.DB_DATABASE, // Ej: hack_academy_db
-  process.env.DB_USERNAME, // Ej: root
-  process.env.DB_PASSWORD, // Ej: root
-  {
-    host: process.env.DB_HOST, // Ej: 127.0.0.1
-    dialect: process.env.DB_CONNECTION, // Ej: mysql
-    dialectModule: require("pg"),
-    logging: false, // Para que no aparezcan mensajes en consola.
-  }
+	process.env.DB_DATABASE, // Ej: hack_academy_db
+	process.env.DB_USERNAME, // Ej: root
+	process.env.DB_PASSWORD, // Ej: root
+	{
+		host: process.env.DB_HOST, // Ej: 127.0.0.1
+		dialect: process.env.DB_CONNECTION, // Ej: mysql
+		// dialectModule: require("pg"),
+		logging: false, // Para que no aparezcan mensajes en consola.
+	}
 );
 
 const Category = require("./Category")(sequelize, Model);
@@ -26,11 +26,11 @@ Category.hasMany(Product);
 Product.belongsTo(Category);
 
 module.exports = {
-  sequelize,
-  User,
-  Product,
-  Category,
-  Order,
-  NewRelease,
-  Address,
+	sequelize,
+	User,
+	Product,
+	Category,
+	Order,
+	NewRelease,
+	Address,
 };
