19699009c0c203ea5b10e07317adb862cbfb5557
mauroraviolo23 <mauro.raviolo@gmail.com>
2022-04-07 16:37:13 -0300

    Adición Recibido

--- models/Order.js
+++ models/Order.js
@@ -1,45 +1,45 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize, Model) => {
-  class Order extends Model {}
+	class Order extends Model {}
 
-  Order.init(
-    {
-      id: {
-        type: DataTypes.BIGINT.UNSIGNED,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      productList: {
-        type: DataTypes.JSON,
-        allowNull: false,
-      },
-      userName: {
-        type: DataTypes.JSON,
-        allowNull: false,
-      },
-      status: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        defaultValue: "Procesando",
-        validate: {
-          isIn: [["Procesando", "Enviado"]],
-        },
-      },
-      address: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      totalPrice: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-    },
-    {
-      sequelize,
-      modelName: "order",
-    }
-  );
+	Order.init(
+		{
+			id: {
+				type: DataTypes.BIGINT.UNSIGNED,
+				primaryKey: true,
+				autoIncrement: true,
+			},
+			productList: {
+				type: DataTypes.JSON,
+				allowNull: false,
+			},
+			userName: {
+				type: DataTypes.JSON,
+				allowNull: false,
+			},
+			status: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				defaultValue: "Procesando",
+				validate: {
+					isIn: [["Procesando", "Enviado", "Recibido"]],
+				},
+			},
+			address: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			totalPrice: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+		},
+		{
+			sequelize,
+			modelName: "order",
+		}
+	);
 
-  return Order;
+	return Order;
 };
