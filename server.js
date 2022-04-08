03b9179fb3f092975d9762f049ce42953ab63a3e
Charlygaf <charlygaf@gmail.com>
2022-04-08 13:24:55 -0300

    .

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
+
 routes(app);
 
-//dbInitialSetup(); // Comentar para no correr DB cada vez.
+// dbInitialSetup(); // Comentar para no correr DB cada vez.
 
 app.listen(APP_PORT, () =>
   console.log(`\n[Express] Servidor corriendo en el puerto ${APP_PORT}!\n`)
