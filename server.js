63d5b46d44a0096fde7a88bdb7700cfaae35164f
Nicolas Del Valle <adrianchelo7@gmail.com>
2022-04-08 17:30:37 -0300

    arreglo

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const routes = require("./routes");
 const dbInitialSetup = require("./dbInitialSetup");
-const APP_PORT = process.env.APP_PORT || 4100;
+const PORT = process.env.PORT || 4100;
 const app = express();
 const cors = require("cors");
 
@@ -13,5 +13,5 @@ routes(app);
 //dbInitialSetup(); // Comentar para no correr DB cada vez.
 
 app.listen(APP_PORT, () =>
-  console.log(`\n[Express] Servidor corriendo en el puerto ${APP_PORT}!\n`)
+  console.log(`\n[Express] Servidor corriendo en el puerto ${PORT}!\n`)
 );
